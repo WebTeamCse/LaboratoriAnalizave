845768f9c8798597895135d4a4eea1d7a2e27e85
ViganRamadani <63263005+ViganRamadani@users.noreply.github.com>
2020-05-26 21:20:16 +0200

    Ndryshimet rreth faqes kryesore
    nav bar responsive dhe ndryshime thera dizajni

---
+++ scriptFP.js
@@ -0,0 +1,10 @@
+/*src="https://code.jquery.com/jquery-3.2.1.min.js"
+integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+crossorigin="anonymous">
+ 
+ 
+ $('.toggle').click(function() {
+    
+    $('.side-nav').toggleClass('side-nav-open', 500);
+    $(this).toggleClass('open');
+});*/
