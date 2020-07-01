028c877a8038d450b2e3538fae6c16ff0a7aa4aa
tinanee661 <63599995+tinanee661@users.noreply.github.com>
2020-07-01 21:58:52 +0200

    changes

--- footer.php
+++ footer.php
@@ -1,48 +1,52 @@
 <html>
 <footer>
-            
-            <ul >
-               <li><a href="#"><img src="img/instagram.png"></a></li>
-               <li><a href="#"><img src="img/facebook.png"></a></li>
-               <li><a href="#"><img src="img/wifi.png"></a></li>   
-           </ul >
-           <div>
-           <p>Copyright © 2020 Laboratori i Analizave</p>
-           </div>    
-       </footer>
-       </html>
-       <style>
-           footer{
-  display: block;
-  height: 20vh;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-  background: #262626;
 
-}
-footer p{
-  color: white;
-}
-footer ul{
-  width: 500px;
-  padding-left: 254px;
-  margin-left: auto ;
-  margin-right: auto;
-  display: flex;
-}
-footer ul li{
-  position: relative;
-  display: block;
-  color: #666;
-  font-size: 30px;
-  height: 50px;
-  width: 50px;
-  background: #262626;
-  line-height: 60px;
-  border-radius: 50%;
-  margin: 0 15px;
-  cursor: pointer;
+  <ul>
+    <li><a href="#"><img src="img/instagram.png"></a></li>
+    <li><a href="#"><img src="img/facebook.png"></a></li>
+    <li><a href="#"><img src="img/wifi.png"></a></li>
+  </ul>
+  <div>
+    <p>Copyright © 2020 Laboratori i Analizave</p>
+  </div>
+</footer>
 
-}
-       </style>
+</html>
+<style>
+  footer {
+    display: block;
+    height: 20vh;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    background: #262626;
+
+  }
+
+  footer p {
+    color: white;
+  }
+
+  footer ul {
+    width: 500px;
+    padding-left: 254px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+  }
+
+  footer ul li {
+    position: relative;
+    display: block;
+    color: #666;
+    font-size: 30px;
+    height: 50px;
+    width: 50px;
+    background: #262626;
+    line-height: 60px;
+    border-radius: 50%;
+    margin: 0 15px;
+    cursor: pointer;
+
+  }
+</style>
