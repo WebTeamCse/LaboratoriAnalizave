1c2cae90deee918dbb00630334a6f49ad5c154c8
ViganRamadani <63263005+ViganRamadani@users.noreply.github.com>
2020-07-02 13:06:20 +0200

    some changes

--- footer.php
+++ footer.php
@@ -1,52 +1,57 @@
 <html>
 <footer>
+            
+            <ul >
+               <li><a href="#"><img src="../img/instagram.png"></a></li>
+               <li><a href="#"><img src="../img/facebook.png"></a></li>
+               <li><a href="#"><img src="../img/wifi.png"></a></li>   
+           </ul >
+           <div>
+           <p>Copyright © 2020 Laboratori i Analizave</p>
+           </div>    
+       </footer>
+       </html>
+       <style>
+           footer{
+  display: block;
+  height: 20vh;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  background: #262626;
 
-  <ul>
-    <li><a href="#"><img src="img/instagram.png"></a></li>
-    <li><a href="#"><img src="img/facebook.png"></a></li>
-    <li><a href="#"><img src="img/wifi.png"></a></li>
-  </ul>
-  <div>
-    <p>Copyright © 2020 Laboratori i Analizave</p>
-  </div>
-</footer>
+}
+footer p{
+  color: white;
+ text-align:center;
+}
+footer ul{
+  width: 14%;
+  
+  margin-left: auto ;
+  margin-right: auto;
+  display: flex;
+  padding-top: 35px;
+}
+footer ul li{
+  position: relative;
+  vertical-align: middle;
+  display: block;
+  color: #666;
+  font-size: 30px;
+  padding-left:0px;
+  height: 50px;
+  width: 31%;
+  background: #262626;
+  line-height: 60px;
+  border-radius: 50%;
+  margin: 0 5px;
+  cursor: pointer;
 
-</html>
-<style>
-  footer {
-    display: block;
-    height: 20vh;
-    text-align: center;
-    align-items: center;
-    justify-content: center;
-    background: #262626;
-
+}
+@media (max-width:900px){
+  footer ul{
+    width:37%;
   }
-
-  footer p {
-    color: white;
-  }
-
-  footer ul {
-    width: 500px;
-    padding-left: 254px;
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-  }
-
-  footer ul li {
-    position: relative;
-    display: block;
-    color: #666;
-    font-size: 30px;
-    height: 50px;
-    width: 50px;
-    background: #262626;
-    line-height: 60px;
-    border-radius: 50%;
-    margin: 0 15px;
-    cursor: pointer;
-
-  }
-</style>
+}
+       </style>
