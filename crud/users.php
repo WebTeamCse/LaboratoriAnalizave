eb1f3362a0c3f004b5dd13a2e415fd249be60eb8
ViganRamadani <vr45297@ubt-uni.net>
2020-07-03 20:24:53 +0200

    Crud show

---
+++ crud/users.php
@@ -0,0 +1,31 @@
+<?php
+    require 'dbconfig.php';
+
+    $query = $conn->query('SELECT * FROM users');
+
+    $users = $query->fetchAll();
+
+?>
+<div class="container">
+    <h1><a href="add_users.php"></a></h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Emri</th>
+                <th>E-mail</th>
+                <th>Funksionet</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        <?php foreach($users as $user):?>
+        <tr>
+            <td><?php echo $user['name']; ?></td>
+            <td><?php echo $user['email']; ?></td>
+            <td>Edit | Delete</td>
+        </tr>
+        <?php endforeach; ?>
+        
+        </tbody>
+    </table>
+</div>
