1796b34eb993514003c4ff90defec5e8cfc6d500
tinanee661 <63599995+tinanee661@users.noreply.github.com>
2020-07-06 20:06:00 +0200

    changes

--- Location/Location.php
+++ Location/Location.php
@@ -3,82 +3,79 @@
 
 <head>
   <title>Page Title</title>
-  <link rel="stylesheet"  href="Locaton.css">
+  <link rel="stylesheet" href="Locaton.css">
 </head>
 
 
 <body>
-<?php
+  <?php
  include "../header.php";
     ?>
   <div class="X">
-  
+
     <ul class="doctorlist">
       <li>
-      <div class="fotodoctor"></div>
-      <div class="description">
-        <h2>Peje</h2>
-        <p> Rruga "Adem Jashari"</p>
-      </div>
+        <div class="fotodoctor"></div>
+        <div class="description">
+          <h2>Peje</h2>
+          <p> Rruga "Adem Jashari"</p>
+        </div>
       </li>
       <li>
-      <div class="fotodoctor"></div>
-      <div class="description">
-        <h2>Gjakove</h2>
-        <p>Rruga "Adem Jashari"</p>
-      </div>
+        <div class="fotodoctor"></div>
+        <div class="description">
+          <h2>Gjakove</h2>
+          <p>Rruga "Adem Jashari"</p>
+        </div>
       </li>
       <li>
-      <div class="fotodoctor"></div>
-      <div class="description">
-        <h2>Lipjan</h2>
-       <a href="https://www.google.com/maps"> Rruga "Adem Jashari"</a>   
-       <a href="#"> a </a>
-      </div>
+        <div class="fotodoctor"></div>
+        <div class="description">
+          <h2>Lipjan</h2>
+          <a href="https://www.google.com/maps"> Rruga "Adem Jashari"</a>
+          <a href="#"> a </a>
+        </div>
       </li>
-    
+
       <li>
         <div class="fotodoctor"></div>
         <div class="description">
           <h2>Prishtin</h2>
           <p>Rruga "Adem Jashari"</p>
         </div>
-        </li>
-        <li>
-            <div class="fotodoctor"></div>
-            <div class="description">
-              <h2>Ferizaj</h2>
-              <p>Rruga "Adem Jashari"</p>
-            </div>
-            </li>
+      </li>
+      <li>
+        <div class="fotodoctor"></div>
+        <div class="description">
+          <h2>Ferizaj</h2>
+          <p>Rruga "Adem Jashari"</p>
+        </div>
+      </li>
 
 
-            <li>
-                <div class="fotodoctor"></div>
-                <div class="description">
-                  <h2>Kaqanik</h2>
-                  <p></p>
-                </div>
-                </li>
+      <li>
+        <div class="fotodoctor"></div>
+        <div class="description">
+          <h2>Kaqanik</h2>
+          <p></p>
+        </div>
+      </li>
     </ul>
-    
-    
-      
-    </div>
-    <script>
-                
-                $('.toggle').click(function() {
-        
-                            $('.side-nav').toggleClass('side-nav-open', 500);
-                            $(this).toggleClass('open');
-                        });
-                    
-                
-        </script>
-         <?php
-                include "../footer.php";
-            ?>
-        
+
+
+
+  </div>
+  <script>
+    $('.toggle').click(function () {
+
+      $('.side-nav').toggleClass('side-nav-open', 500);
+      $(this).toggleClass('open');
+    });
+  </script>
+  <?php
+    include "../footer.php";
+  ?>
+
 </body>
 
 </html>
