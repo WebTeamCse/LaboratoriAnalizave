bbb89869f277e0d8116ecbc043900c22d13676fe
ViganRamadani <vr45297@ubt-uni.net>
2020-07-09 16:17:48 +0200

    password < 8 rtn false

--- Register/indexRegister.php
+++ Register/indexRegister.php
@@ -13,11 +13,36 @@
         
     </head>
     <body>
-    <?php
-    require '../crud/dbconfig.php';
-    
+   
+    <div class="register">
+        <form class="form" onsubmit="return validate()" id="form" action="indexRegister.php" method="post">
+              <h1>Register</h1>
+              <p> Please login to your account.</p>
+           
+              <input type="text" id="name" name="name" placeholder="Shenoni emrin"><br>
+            <input type="text" name="email" id="email" placeholder="Shenoni email"><br>
+            <input type="text" id="username" name="username" placeholder="Shenoni username"><br>
+            <input type="password" id="password" name="password" placeholder="Shenoni password"><br>
+            <input type="submit" name="submit" value="ruaj shenimet">
+            
+            </div>   
+           
+        </form>
+    </div>
 
    
+    
+     
+        
+        <?php
+            
+            // include "../crud/insert-users.php";
+        ?> 
+    </body>
+ 
+</html>
+<?php
+    require '../crud/dbconfig.php';
 
     if(isset($_POST["submit"])){
         $name = $_POST['name'];
@@ -37,6 +62,13 @@
      }
      //exit
 
+     //password length not lower then 8
+        $passwordlength = strlen($password);
+     if($passwordlength < 8 ){
+        header("Location: indexRegister.php?error=passwordShouldHaveAtLeast8Charactes");
+        exit();
+        //exit
+     }
 
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
@@ -59,45 +91,3 @@
     }
     
     ?>
-    <div class="register">
-        <form class="form" onsubmit="return validate()" id="form" action="indexRegister.php" method="post">
-              <h1>Register</h1>
-              <p> Please login to your account.</p>
-           
-              <input type="text" id="name" name="name" placeholder="Shenoni emrin"><br>
-            <input type="text" name="email" id="email" placeholder="Shenoni email"><br>
-            <input type="text" id="username" name="username" placeholder="Shenoni username"><br>
-            <input type="password" id="password" name="password" placeholder="Shenoni password"><br>
-            <input type="submit" name="submit" value="ruaj shenimet">
-            
-            </div>   
-           
-        </form>
-    </div>
-
-   
-    
-        <!-- <div class="register">
-           
-           
-                <form class="form" id="form" action="indexRegister.php" method="post" onsubmit="return validateForm()">
-                    <h1>Register</h1>
-                    <p>Welcome back! Please login to your account.</p>
-                        <input id="name" name="name" type="text" placeholder="Full Name">
-                        <br>
-                        <input id="email" name="email" type="text" placeholder="Email">
-                        <br>
-                        <input id="password" name="password" type="password" placeholder="Password"><br>
-                        <div class="btn">
-                      
-                    </div>                       
-                </form>           
-        </div>     -->
-        
-        <?php
-            
-            // include "../crud/insert-users.php";
-        ?> 
-    </body>
- 
-</html>
