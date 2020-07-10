e34624c2935c16270652b7b57c78cd3e67d236a3
ViganRamadani <vr45297@ubt-uni.net>
2020-07-10 20:03:06 +0200

    admin can see sms and delete users

--- crud/users.php
+++ crud/users.php
@@ -3,10 +3,11 @@
     require 'dbconfig.php';
     $query = $conn->query('SELECT users.id , 
            users.name , 
-           users.username , 
+           users.username ,
+           users.email , 
            sms.user_id , 
            sms.content
-    FROM users INNER JOIN sms on users.id = sms.user_id');
+    FROM users  INNER JOIN sms on users.id = sms.user_id ');
     $users = $query->fetchAll();
 ?>
     <html>
@@ -14,18 +15,27 @@
 
         </head>
         <body>
+          
 
-        <table id="demo" border='1'>
+        <table class="demo" >
             <tr>
                 <th>User ID</th>
                 <th>User Name</th>
+                <th>User Email</th>
                 <th>User SMS</th>
+                <th>Edit User</th> 
+                <!-- <th>Delete User</th> -->
+                          
             </tr>
         <?php foreach($users as $user):?>
         <tr>
             <td><?php echo $user['id'];?></td>
             <td><?php echo $user['name'];?></td>
+            <td><?php echo $user['email'];?></td>
             <td><?php echo $user['content'];?></td>
+            <td class="edit"><a href=""><?php echo "Edit this user"?></a></td>
+            <!-- <td class="delete"><a href="delete_user.php><?php echo "Delete this user"?></a></td> -->
+
         </tr>
         <?php endforeach;?>
 </table>
